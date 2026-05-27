3f34a12b72b6fa0bc77e89f3f9b2994b7e10c56b
ankushagarwala201221-code <ankushagarwala201221@gmail.com>
2026-05-27 21:08:05 +0530

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -1,1 +1,64 @@
+import { db }
 
+from "./firebase-config.js";
+
+import {
+
+ref,
+onValue
+
+}
+
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const alertBox =
+document.getElementById("alertText");
+
+const vriBox =
+document.getElementById("vri");
+
+const pmBox =
+document.getElementById("pm");
+
+onValue(ref(db,"AERIS/live"),(snap)=>{
+
+const d = snap.val();
+if(!d) return;
+
+/* ALERT LOGIC */
+
+let alertMsg = "System Stable";
+
+if(d.vri < 40){
+
+alertMsg = "⚠ Severe visibility drop";
+
+}
+
+if(d.pm > 150){
+
+alertMsg = "⚠ Dangerous pollution level";
+
+}
+
+if(d.rain == 1){
+
+alertMsg = "🌧 Rain detected";
+
+}
+
+if(d.hum > 90){
+
+alertMsg = "💧 High humidity risk";
+
+}
+
+/* UPDATE */
+
+if(alertBox){
+
+alertBox.innerHTML = alertMsg;
+
+}
+
+});
