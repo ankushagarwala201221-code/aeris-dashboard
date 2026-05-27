5488a5fa96c3720efb430b29449a95ab5d6cb24f
ankushagarwala201221-code <ankushagarwala201221@gmail.com>
2026-05-27 21:08:53 +0530

    Update prediction.js

--- prediction.js
+++ prediction.js
@@ -1,1 +1,54 @@
+import { db }
 
+from "./firebase-config.js";
+
+import {
+
+ref,
+onValue
+
+}
+
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const predictionBox =
+document.getElementById("predictionText");
+
+onValue(ref(db,"AERIS/live"),(snap)=>{
+
+const d = snap.val();
+if(!d) return;
+
+let msg = "Atmosphere Stable";
+
+if(d.pm > 120){
+
+msg = "⚠ Pollution may increase soon";
+
+}
+
+if(d.hum > 85){
+
+msg = "🌫 Fog/low visibility possible";
+
+}
+
+if(d.rain == 1){
+
+msg = "🌧 Rain improving air quality";
+
+}
+
+if(d.temp > 38){
+
+msg = "🔥 Heat stress conditions possible";
+
+}
+
+if(predictionBox){
+
+predictionBox.innerHTML = msg;
+
+}
+
+});
