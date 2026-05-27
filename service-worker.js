560d3150fbd984f62fa68057127778898d846202
ankushagarwala201221-code <ankushagarwala201221@gmail.com>
2026-05-27 23:15:39 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,56 +1,40 @@
-const CACHE_NAME = "aeris-v2";
+const CACHE_NAME = "aeris-v3";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./dashboard.html",
-  "./dashboard.css",
-  "./dashboard.js",
-  "./charts.js",
-  "./style.css",
-  "./profile.html",
-  "./profile.css",
-  "./profile.js",
-  "./admin.html",
-  "./admin.css",
-  "./admin.js",
-  "./signup.html",
-  "./prediction.js",
-  "./alert.js",
-  "./app.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "/aeris-dashboard/",
+  "/aeris-dashboard/index.html",
+  "/aeris-dashboard/dashboard.html",
+  "/aeris-dashboard/dashboard.css",
+  "/aeris-dashboard/dashboard.js",
+  "/aeris-dashboard/charts.js",
+  "/aeris-dashboard/style.css",
+  "/aeris-dashboard/profile.html",
+  "/aeris-dashboard/profile.css",
+  "/aeris-dashboard/profile.js",
+  "/aeris-dashboard/admin.html",
+  "/aeris-dashboard/admin.css",
+  "/aeris-dashboard/admin.js",
+  "/aeris-dashboard/signup.html",
+  "/aeris-dashboard/manifest.json",
+  "/aeris-dashboard/icon-192.png",
+  "/aeris-dashboard/icon-512.png"
 ];
 
-self.addEventListener("install",(event)=>{
-
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-    .then((cache)=>{
-      return cache.addAll(urlsToCache);
-    })
+      .then(cache => cache.addAll(urlsToCache))
   );
-
   self.skipWaiting();
-
 });
 
-self.addEventListener("activate",(event)=>{
-
-  event.waitUntil(
-    clients.claim()
-  );
-
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
 });
 
-self.addEventListener("fetch",(event)=>{
-
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request)
-    .then((response)=>{
-      return response || fetch(event.request);
-    })
+      .then(response => response || fetch(event.request))
   );
-
 });
