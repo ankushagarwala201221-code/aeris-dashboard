7016693b4120b3f2fb82d22352dee58b7903d3fc
ankushagarwala201221-code <ankushagarwala201221@gmail.com>
2026-05-27 22:56:47 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,56 @@
-const CACHE_NAME = "aeris-v1";
+const CACHE_NAME = "aeris-v2";
 
 const urlsToCache = [
-
-"./",
-"./dashboard.html",
-"./dashboard.css",
-"./dashboard.js",
-"./charts.js",
-"./style.css",
-"./profile.css",
-"./profile.js",
-"./admin.css",
-"./admin.js",
-"./prediction.js",
-"./alert.js",
-"./app.js"
-
+  "./",
+  "./index.html",
+  "./dashboard.html",
+  "./dashboard.css",
+  "./dashboard.js",
+  "./charts.js",
+  "./style.css",
+  "./profile.html",
+  "./profile.css",
+  "./profile.js",
+  "./admin.html",
+  "./admin.css",
+  "./admin.js",
+  "./signup.html",
+  "./prediction.js",
+  "./alert.js",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install",(event)=>{
 
-event.waitUntil(
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then((cache)=>{
+      return cache.addAll(urlsToCache);
+    })
+  );
 
-caches.open(CACHE_NAME)
-.then((cache)=>cache.addAll(urlsToCache))
+  self.skipWaiting();
 
-);
+});
+
+self.addEventListener("activate",(event)=>{
+
+  event.waitUntil(
+    clients.claim()
+  );
 
 });
 
 self.addEventListener("fetch",(event)=>{
 
-event.respondWith(
-
-caches.match(event.request)
-.then((response)=>{
-
-return response || fetch(event.request);
-
-})
-
-);
+  event.respondWith(
+    caches.match(event.request)
+    .then((response)=>{
+      return response || fetch(event.request);
+    })
+  );
 
 });
