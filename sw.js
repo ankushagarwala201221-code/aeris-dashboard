b45f85a61b12d3732e84bff8c983dc34006f4f29
ankushagarwala201221-code <ankushagarwala201221@gmail.com>
2026-05-28 11:33:45 +0530

    Update and rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "aeris-cache-v1";
+
+const urlsToCache = [
+"index.html",
+"dashboard.html",
+"admin.html",
+"profile.html",
+"dashboard.css",
+"dashboard.js",
+"charts.js",
+"info.js"
+];
+
+self.addEventListener("install",event=>{
+
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache=>{
+return cache.addAll(urlsToCache);
+})
+);
+
+});
+
+self.addEventListener("fetch",event=>{
+
+event.respondWith(
+caches.match(event.request)
+.then(response=>{
+return response || fetch(event.request);
+})
+);
+
+});
